33300f25c820aa1d91d4d66b79163c011fc914b4
Pandora417 <pq1580830988@163.com>
2020-04-07 18:02:59 +0800

    mock&接口

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import storage from "./storage/index"
+// import storage from "./storage/index"
 export default {
   name: 'App',
   components: {
@@ -17,8 +17,7 @@ export default {
     }
   },
   mounted(){
-    // storage.setItem('b',"2")
-    storage.clear("b")
+ 
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import Router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
+// import env from './env'
 
-Vue.config.productionTip = false
+//mock开关
+const mock = false;
+if(mock){
+  require('./mock/api');
+}
+
+//根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;//超时会终止
+//接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if (res.status == 0){ //正常
+    return res.data;
+  }else if(res.status == 10){//状态码异常，未登录
+    window.location.href = '/#/login';
+  }else{
+    alert(res.msg);
+  }
+})
+Vue.use(VueAxios,axios)
+Vue.config.productionTip = false //生产环境的提示
 
 new Vue({
   router:Router,
