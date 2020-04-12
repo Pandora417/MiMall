607b9d14c7b1258b38928cb46938dcd122dd514e
Pandora417 <pq1580830988@163.com>
2020-04-12 11:02:35 +0800

    服务组件

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,11 +3,13 @@
     <nav-header></nav-header>
     <router-view></router-view>
     <nav-footer></nav-footer>
+    
   </div>
 </template>
 <script>
 import NavHeader from './../components/NavHeader'
 import NavFooter from './../components/NavFooter'
+
 export default{
   name:'home',
   components:{
