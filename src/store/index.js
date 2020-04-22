03c50e0af78e1871d6a45d2a814b27ef0f6a7660
Pandora417 <pq1580830988@163.com>
2020-04-22 16:26:00 +0800

    解决刷新后cartList为空数组的问题

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,10 @@ import Vuex from 'vuex'
 import mutations from './mutations'
 import actions from './actions'
 Vue.use(Vuex)
-//为了缓存
+
+//为了刷新后数据依旧还是缓存的数据
 let defaultName = '';
-// let defaultCart = 0;
+let defaultCartList = [];
 try {
   if (localStorage.username) {
     defaultName = localStorage.username
@@ -14,18 +15,18 @@ try {
 } catch (e) {
   alert("请重新输入")
 }
-// try {
-//   if (localStorage.cartCount) {
-//     defaultCart = localStorage.cartCount
-//   }
-// } catch (e) {
-//   alert("请重新输入")
-// }
+try {
+  if (localStorage.cartList) {
+    defaultCartList = JSON.parse(localStorage.cartList)
+  }
+} catch (e) {
+  alert("请重新输入")
+}
 
 const state={
   username:defaultName, //登录用户名
   cartCount:0,  //购物车数量
-  cartList:[]
+  cartList:defaultCartList
 }
 
 export default new Vuex.Store({
